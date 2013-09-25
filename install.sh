a23f33df261ba9bbd2fa1b0dbcd5472130f158d1
Steven Cai <stevencaiyaohua@gmail.com>
2013-09-25 22:13:39 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ tmp_dir=`pwd`/tools/tmp
 mkdir ${tmp_dir}
 
 #
-# Emacs plugin
+# Emacs
 #
-echo "Installing Emacs plugins..."
-echo "        Config Emacs..."
+echo "Install and config Emacs..."
+echo "        Configing Emacs..."
 cd ${pwd}/emacs
 cp -rf .emacs ~/
 cp -rf .emacs.d ~/
@@ -18,12 +18,12 @@ echo "        Installing Cscope..."
 cd ${pwd}/tools
 tar -xzf cscope* -C ${tmp_dir}
 cd ${tmp_dir}/cscope*
-./configure
-make
-sudo make install
+./configure > /dev/null
+make > /dev/null
+sudo make install > /dev/null
 sudo cp cscope-indexer /usr/bin
 sudo chmod 755 /usr/bin/cscope-indexer
-
+sed -i 's/cscope -b -i $LIST_FILE -f $DATABASE_FILE/cscope -q -b -i $LIST_FILE -f $DATABASE_FILE/' /usr/bin/cscope-indexer
 
 
 rm -rf ${tmp_dir}
