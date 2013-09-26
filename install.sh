eb0791ffbca5df892345b0bffd7a90b25bf7e533
Steven Cai <stevencaiyaohua@gmail.com>
2013-09-27 00:32:11 +0800

    Install LaTex and enable auctex for Emacs

--- install.sh
+++ install.sh
@@ -4,10 +4,27 @@ pwd=`pwd`
 tmp_dir=`pwd`/tools/tmp
 black_hole="/dev/null 2>&1"
 
-echo ${black_hole}
-
 # dead work
 mkdir ${tmp_dir}
+
+function make_install()
+{
+    cd ${pwd}/tools
+    tar -xzf $1 -C ${tmp_dir}
+    cd ${tmp_dir}/$1
+    ./configure > ${black_hole}
+    make > ${black_hole}
+    sudo make install > ${black_hole}
+    cd ${pwd}
+}
+
+#
+# Latex
+#
+echo "Install LaTex..."
+sudo apt-get install texlive-latex-base
+sudo apt-get install latex-cjk-all
+sudo apt-get install texlive-latex-extra
 
 #
 # Emacs
@@ -17,16 +34,16 @@ echo "        Configing Emacs..."
 cd ${pwd}/emacs
 cp -rf .emacs ~/
 cp -rf .emacs.d ~/
+
 echo "        Installing Cscope..."
-cd ${pwd}/tools
-tar -xzf cscope* -C ${tmp_dir}
-cd ${tmp_dir}/cscope*
-./configure > ${black_hole}
-make > ${black_hole}
-sudo make install > ${black_hole}
+make_install cscope*
 sudo cp ${tmp_dir}/cscope*/contrib/xcscope/cscope-indexer /usr/bin
 sudo chmod 755 /usr/bin/cscope-indexer
 sed -i 's/cscope -b -i $LIST_FILE -f $DATABASE_FILE/cscope -q -b -i $LIST_FILE -f $DATABASE_FILE/' /usr/bin/cscope-indexer
 
+echo "        Installing auctex..."
+make_install auctex*
 
 rm -rf ${tmp_dir}
+
+exit 0
