98344da46dce5d0c062e13a73466bb04ed0a7cbb
Steven Cai <stevencaiyaohua@gmail.com>
2020-04-16 21:10:02 +1200

    emacs: add package-archives lists

--- config/.emacs.d/init.el
+++ config/.emacs.d/init.el
@@ -1,8 +1,16 @@
 (require 'package)
+
 (add-to-list 'package-archives
-;; '("marmalade" . "http://marmalade-repo.org/packages/") ;
-	     '("melpa" . "https://melpa.org/packages/"))
-;;        '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+	'("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+	'("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives
+	'("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	'("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+	'("gnu-mirror" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu") t)
+
 
 (package-initialize) ;; You might already have this line
 
